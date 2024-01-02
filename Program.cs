dda37c48d4d44c5bfcbc54943f81ddcef931d8b7
Thaís Alessandra <101782983+Thaisalessandra@users.noreply.github.com>
2024-01-02 14:47:32 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
+//--------IPHONE------------------
+Console.WriteLine("--------------IPHONE----------------");
+
+Iphone i = new Iphone("55",3541,5555555,128);
+i.InstalarAplicativo("Chrome");
+i.ReceberLigacao();
+i.Ligar();
+
+//--------------NOKIA-----------------
+Console.WriteLine("--------------NOKIA-----------------");
+Nokia nokia = new Nokia("3541",556,3542,64);
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+nokia.Ligar();
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
